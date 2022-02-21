d0b3637ff8a9370b7451d697f8226569b05ac92d
Paxxs <10364775+Paxxs@users.noreply.github.com>
2022-02-21 17:44:51 +0800

    fix go mod name typo
    close #3

--- get_real_ip_test.go
+++ get_real_ip_test.go
@@ -6,7 +6,7 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	plugin "github.com/paxxs/traefik-get-real-ip"
+	plugin "github.com/Paxxs/traefik-get-real-ip"
 )
 
 func TestNew(t *testing.T) {
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/paxxs/traefik-get-real-ip
+module github.com/Paxxs/traefik-get-real-ip
 
 go 1.13
